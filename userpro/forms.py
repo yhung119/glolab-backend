12eff4ebc65b1dee64f7eb3c6b063a8c352b3e0e
fongbb132 <fo_og@hotmail.com>
2015-07-28 01:35:14 -0400

    global

--- userpro/forms.py
+++ userpro/forms.py
@@ -35,6 +35,33 @@ class UserProfileForm(forms.ModelForm):
 
 		return user
 
+class editStudentProfile(forms.ModelForm):
+	username = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+    def clean_email(self):
+        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
+
+        if email and User.objects.filter(email=email).exclude(username=username).count():
+            raise forms.ValidationError('This email address is already in use. Please supply a different email address.')
+        return email
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
+
 class CompanyProfileForm(forms.ModelForm):
 	company_name = forms.CharField(max_length=128, required=True)
 	description = forms.CharField(label='', 
